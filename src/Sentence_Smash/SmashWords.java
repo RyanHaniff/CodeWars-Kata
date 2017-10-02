5191958e93d8eca72710b4dfb5844bbc5c8a11b3
RyanHaniff <haniffryan.hr@gmail.com>
2017-10-02 10:13:42 -0400

    Simplified way of combining words togather. Using the String classes join() method.

--- src/Sentence_Smash/SmashWords.java
+++ src/Sentence_Smash/SmashWords.java
@@ -12,20 +12,7 @@ public class SmashWords {
 
     public static String smash(String... words) {
 
-        String join = "";
-
-        for (int i = 0; i < words.length; i++) {
-            join = join + words[i];
-            if (i != words.length - 1) {
-                join = join + " ";
-            }
-
-        }
-
-        //Or you can do this:
-        //return String.join(" ", words);
-
-        return join;
+        return String.join(" ", words);
 
     }
 }
