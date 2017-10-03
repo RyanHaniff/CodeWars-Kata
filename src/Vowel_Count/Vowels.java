de009b1a53f38425f1d3d7594fdd035218dd0ba9
RyanH <haniffryan.hr@gmail.com>
2017-10-03 16:53:09 -0400

    Initial commit

---
+++ src/Vowel_Count/Vowels.java
@@ -0,0 +1,41 @@
+package Vowel_Count;
+
+public class Vowels {
+
+    public static void main(String[] args) {
+
+        System.out.println(getCount("aaaeigflo"));
+    }
+
+    public static int getCount(String str) {
+        int vowelsCount = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            switch (str.charAt(i)) {
+                case 'a':
+                    vowelsCount++;
+                    break;
+
+                case 'e':
+                    vowelsCount++;
+                    break;
+
+                case 'i':
+                    vowelsCount++;
+                    break;
+
+                case 'o':
+                    vowelsCount++;
+                    break;
+
+                case 'u':
+                    vowelsCount++;
+                    break;
+
+            }
+        }
+        return vowelsCount;
+    }
+
+}
