ebfbca20ead3c0edb63d5de13bb197d75a246543
RyanHaniff <haniffryan.hr@gmail.com>
2017-10-03 14:38:19 -0400

    Added simplified version

--- src/Most_Digits/Digits.java
+++ src/Most_Digits/Digits.java
@@ -27,5 +27,15 @@ public class Digits {
 
         return largestValue;
 
+//        public static int findLongest(int[] numbers) {
+//            int index = 0;
+//            for (int i = 0; i < numbers.length; i++) {
+//                if (String.valueOf(Math.abs(numbers[i])).length() > String.valueOf(Math.abs(numbers[index])).length()) {
+//                    index = i;
+//                }
+//            }
+//            return numbers[index];
+//        }
+
     }
 }
