070bac22272f3ac794f0d1a63c4f2c9075c256fb
RyanHaniff <haniffryan.hr@gmail.com>
2017-10-03 13:58:08 -0400

    Added simplified version in comments

--- src/Name_Array_Capping/Array_Capping.java
+++ src/Name_Array_Capping/Array_Capping.java
@@ -45,5 +45,16 @@ public class Array_Capping {
         }
 
         return str;
+
+//        public static String[] capMe(String[] strings) {
+//            for (int i = 0; i < strings.length; i++) {
+//                if (strings.length > 0)
+//                    strings[i] = Character.toUpperCase(strings[i].charAt(0)) +
+//                            strings[i].substring(1, strings[i].length()).toLowerCase();
+//            }
+//            return strings;
+//        }
+
+
     }
 }
