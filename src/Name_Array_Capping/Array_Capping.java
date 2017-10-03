7fc594a4f6c25846d7eac56f9c4823e6ac550247
RyanHaniff <haniffryan.hr@gmail.com>
2017-10-03 11:38:12 -0400

    Saved before removing for each loop

--- src/Name_Array_Capping/Array_Capping.java
+++ src/Name_Array_Capping/Array_Capping.java
@@ -5,26 +5,33 @@ public class Array_Capping {
 
     public static void main(String[] args) {
 
-        String[] arr = {"hello", "ryan"};
+        String[] arr = {"heLLo", "ryan"};
 
-        capMe(arr);
+        System.out.println(capMe(arr));
     }
 
     public static String[] capMe(String[] strings) {
 
-        for(String a : strings) {
+        StringBuilder sb = new StringBuilder();
+        for (String word : strings) { //looping through string array a = 0 we are at hello
 
-            char[] test = a.toCharArray();
-            char b = Character.toUpperCase(test[0]);
+//            char[] charArray = word.toCharArray();
+//            char b = Character.toUpperCase(charArray[0]);
 
-            StringBuilder sb = new StringBuilder();
-            for(int i = 0; i < test.length; i++){
-                sb.append(test[i]);
+            sb.append(Character.toUpperCase(word.charAt(0)));
+            for (int i = 1; i < word.length(); i++) {
+                if (Character.isUpperCase(word.charAt(i))) {
+                    sb.append(Character.toLowerCase(word.charAt(i)));
+                } else {
+                    sb.append(word.charAt(i));
+                }
+
+                strings[i - 1] = sb.toString();
             }
-            System.out.println(sb);
+
+
         }
 
-        return null;
-
+        return strings;
     }
 }
